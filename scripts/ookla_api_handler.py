b142123932949fe1a30bb480229421bc3f201991
Charly Marchiaro <carlosmarchiaro@gmail.com>
2022-07-08 14:55:04 -0300

    using requests instead of urllib

--- scripts/ookla_api_handler.py
+++ scripts/ookla_api_handler.py
@@ -1,8 +1,9 @@
+import io
 import os
 
 import logging
 
-import urllib.request
+import requests
 import json
 import base64
 import sys
@@ -18,24 +19,23 @@ def list_remote_android_bg_v2_files() -> list[any]:
     username = os.environ.get("DATA_EXTRACTS_API_KEY_ID")
     password = os.environ.get("DATA_EXTRACTS_API_KEY_SECRET")
 
-    opener = urllib.request.build_opener()
-    urllib.request.install_opener(opener)
-    opener.addheaders = [("Accept", "application/json")]
-
     # setup authentication
     login_credentials = "%s:%s" % (username, password)
     base64string = base64.b64encode(login_credentials.encode("utf-8")).decode("ascii")
-    opener.addheaders = [("Authorization", "Basic %s" % base64string)]
 
     # makes request for files
     try:
-        response = urllib.request.urlopen(extracts_url).read()
-    except urllib.request.HTTPError as error:
+        s = requests.Session()
+        s.headers.update({"Accept": "application/json"})
+        s.headers.update({"Authorization": "Basic %s" % base64string})
+        response = s.get(extracts_url)
+
+    except Exception as error:
         logging.error(error)
         sys.exit()
 
     try:
-        items = json.loads(response)
+        items = json.loads(response.text)
         return items
 
     except ValueError as error:
@@ -59,14 +59,11 @@ def select_remote_android_bg_v2_files(dates_str: list[str]) -> list[any]:
 
 def download_and_unzip_file(file: any) -> str:
     logging.info("Downloading " + file["name"] + "...")
-    response = urllib.request.urlopen(file["url"])
-    zip_file_path = consts.tmp_folder + "/" + file["name"]
-    with open(zip_file_path, "wb") as f:
-        f.write(response.read())
-
+    response = requests.get(file["url"])
     logging.info("Extracting...")
+    zip_file_path = consts.tmp_folder + "/" + file["name"]
     csv_file_name = None
-    with ZipFile(zip_file_path, "r") as zip:
+    with ZipFile(io.BytesIO(response.content)) as zip:
         csv_file_name = zip.namelist()[0]
         zip.extractall(path=consts.tmp_folder)
 
